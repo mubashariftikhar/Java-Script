696515a65ae2d5a847dacd3b86df5ed3711f57bd
Mubashar Iftikhar <62559139+mubashariftikhar@users.noreply.github.com>
2024-02-27 17:47:17 +0500

    js...

---
+++ modules/library.js
@@ -0,0 +1,22 @@
+// export let message = "ES6 Modules";
+
+// export function user(name) {
+//   console.log(`Heloo ${name}`);
+// }
+
+// export class test {
+//   constructor() {
+//     console.log("Hello I am Constructor");
+//   }
+// }
+// --------------------export { message,user,test};
+
+// export default function () {
+//   console.log("Helloo G");
+// }
+
+///------------------
+
+export function user() {
+  console.log("Helloo G");
+}
---
+++ modules/main.js
@@ -0,0 +1,16 @@
+// import { message, user, test } from "./library.js";
+
+// console.log(message);
+
+// // document.body.innerHTML = message;
+
+// user("Mohid Khan");
+
+// let a = new test();
+
+// 1---import { default as mohid } from "./library.js";
+// 2---import mohid from "./library.js";
+// mohid();
+
+import { user } from "./bridge.js";
+user();
